df613c549db8c25de7c18d68c5ef9b8372082c58
Alabhya Jindal <alabhya10@gmail.com>
2022-05-22 16:19:47 +0530

    Dark theme.
    Hello World

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,14 @@ export default function Home() {
         <title>Hello</title>
         <meta name='description' content='Hello World' />
       </Head>
-      {/* <h1>Hello World</h1> */}
+      <h1>Hello World</h1>
+      <style jsx>{`
+        h1 {
+          font-size: 5rem;
+          margin-top: 5rem;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
